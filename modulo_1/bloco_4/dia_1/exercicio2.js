50080b0f7757cb4e803574d91cb5210046f2902c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-08-27 18:31:24 -0300

    Segundo Exercicio

---
+++ modulo_1/bloco_4/dia_1/exercicio2.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 5;
+
+if (a > b) {
+  console.log("O maior é o valor a: " + a);
+} else if (b > a) {
+  console.log("O maior é o valor b: " + b);
+} else {
+  console.log("Os numeros são iguais");
+}
