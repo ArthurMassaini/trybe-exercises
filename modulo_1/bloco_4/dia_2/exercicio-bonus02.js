253d5b71cf4f2a3a6ea88e9e8636703b31836812
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-08-28 18:15:34 -0300

    Exercicio Bonus 2

--- modulo_1/bloco_4/dia_2/exercicio-bonus02.js
+++ modulo_1/bloco_4/dia_2/exercicio-bonus02.js
@@ -1,11 +1,10 @@
-// Ordene o array numbers em ordem crescente e imprima seus valores;
-
+// // Ordene o array numbers em ordem decrescente e imprima seus valores
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 1; i < numbers.length; i += 1) {
   for (let j = 0; j < i; j++) {
-    if (numbers[i] < numbers[j]) {
+    if (numbers[i] > numbers[j]) {
       let position = numbers[i];
 
       numbers[i] = numbers[j];
