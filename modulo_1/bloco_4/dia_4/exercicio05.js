0d81486ef60545f08d76d79d9b33336f1c182472
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-01 15:33:25 -0300

    Exercicio 5 - Objetos/For/In

---
+++ modulo_1/bloco_4/dia_4/exercicio05.js
@@ -0,0 +1,24 @@
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores:
+//"Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+for (let i in (info, info2)) {
+  if (info[i] === info2[i]) {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[i] + " e " + info2[i]);
+  }
+}
