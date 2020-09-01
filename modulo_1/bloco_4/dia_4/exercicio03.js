7fc340383a8bde10e2639e8ce0a98479d41d7e44
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-01 15:19:18 -0300

    Exercicio 3 - Objetos/For/In

---
+++ modulo_1/bloco_4/dia_4/exercicio03.js
@@ -0,0 +1,15 @@
+//Faça um for/in que mostre todas as chaves do objeto.
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+j = 0;
+for (let i in info) {
+  if (j == 0) {
+    console.log("Chaves:");
+  }
+  console.log(i);
+  j += 1;
+}
