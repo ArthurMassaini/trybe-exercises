f811c97c1e3d6d9d306bccf69b0eed9c4e60145d
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-01 16:31:52 -0300

    Exercicio 3(Mudando nome da função) - Funções

--- modulo_1/bloco_4/dia_4/exercicio-func03.js
+++ modulo_1/bloco_4/dia_4/exercicio-func03.js
@@ -2,7 +2,7 @@
 //Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 //Valor esperado no retorno da função: 6.
 
-function indiceMaiorValor(array) {
+function indiceMenorValor(array) {
   let menor = array[0];
   let menorIndice = 0;
 
@@ -15,5 +15,5 @@ function indiceMaiorValor(array) {
   return menorIndice;
 }
 
-valor = indiceMaiorValor([2, 3, 6, 7, 10, 0, -3]);
+valor = indiceMenorValor([2, 3, 6, 7, 10, 0, -3]);
 console.log(valor);
