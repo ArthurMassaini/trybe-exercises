6aca97d801dfe495a378ace7c020c524b5b2c8dc
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-04 19:11:11 -0300

    Ultimas mudanças

--- modulo_1/bloco_5/dia_1/script.js
+++ modulo_1/bloco_5/dia_1/script.js
@@ -7,20 +7,47 @@
  //Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
  //Crie uma função que mude a cor do quadrado vermelho para branco.
  //Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+ //Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+ //Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
-let elemento1 = document.getElementsByTagName("p");
-elemento1[0].innerText =
-  "Eu me vejo empregado em uma empresa de tecnologia de sucesso!";
+function mudaParagrafo() {
+  let elemento1 = document.getElementsByTagName("p");
+  elemento1[0].innerText =
+    "Eu me vejo empregado em uma empresa de tecnologia de sucesso!";
+}
+mudaParagrafo();
 
-let elemento2 = document.getElementsByClassName("main-content");
-elemento2[0].style.backgroundColor = "rgb(76,164,109)";
+function mudaCorLateral() {
+  let elemento2 = document.getElementsByClassName("main-content");
+  elemento2[0].style.backgroundColor = "rgb(76,164,109)";
+}
+mudaCorLateral();
 
-let elemento3 = document.getElementsByClassName("center-content");
-elemento3[0].style.backgroundColor = "white";
+function mudaCorCentro() {
+  let elemento3 = document.getElementsByClassName("center-content");
+  elemento3[0].style.backgroundColor = "white";
+}
+mudaCorCentro();
 
-let elemento4 = document.getElementById("titulo");
-elemento4.innerHTML = "Exercício 5.1 - Javascript"; 
+function arrumaTitulo() {
+  let elemento4 = document.getElementById("titulo");
+  elemento4.innerHTML = "Exercício 5.1 - Javascript";
+}
+arrumaTitulo();
 
+function maiusculo() {
+  let elemento5 = document.getElementsByTagName("p");
+  for (let i = 0; i < elemento5.length; i += 1) {
+    elemento5[i].style.textTransform = "uppercase";
+  }
+}
+maiusculo();
+
+function mostrarConteudoParagrafos() {
+  let elemento6 = document.getElementsByTagName("p");
+  for (let j = 0; j < elemento6.length; j += 1) {
+    console.log(elemento6[j].innerHTML);
+  }
+}
+mostrarConteudoParagrafos();
