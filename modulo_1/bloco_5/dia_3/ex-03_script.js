bc33bb581e1414ef3449a6ec39a059db4021c38c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-09 20:36:06 -0300

    Parte 2 - Tarefa Bonus

--- modulo_1/bloco_5/dia_3/ex-03_script.js
+++ modulo_1/bloco_5/dia_3/ex-03_script.js
@@ -24,3 +24,27 @@ button2.addEventListener("click", function () {
   let result = productPrice * discount;
   alert("Desconto de " + result);
 });
+
+//TAREFA 03
+
+let createImage = document.createElement("img");
+createImage.className = "class-image";
+
+let divMain = document.querySelector(".main-div");
+divMain.appendChild(createImage);
+
+let image = document.querySelector(".class-image");
+image.src =
+  "https://leianoticias.com.br/wp-content/uploads/2019/12/pessoasnapraia-43712373f2c106be93ecb080e125d4bb-1200x600-1.jpg";
+
+image.addEventListener("mouseover", function (evento) {
+  image.src =
+    "https://esquerdaonline.com.br/wp-content/uploads/2020/04/terra-57aa8fa715aa3110337592a96d1dadb8-1200x600-1.jpg";
+  console.log(evento.type);
+});
+
+image.addEventListener("mouseout", function (evento) {
+  image.src =
+    "https://leianoticias.com.br/wp-content/uploads/2019/12/pessoasnapraia-43712373f2c106be93ecb080e125d4bb-1200x600-1.jpg";
+  console.log(evento.type);
+});
