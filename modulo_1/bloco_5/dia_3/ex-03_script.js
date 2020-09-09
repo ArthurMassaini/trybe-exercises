120484c64ac4b3139565a0d8e626491fecac8563
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-09 20:21:23 -0300

    Parte 2 - Tarefa 2

--- modulo_1/bloco_5/dia_3/ex-03_script.js
+++ modulo_1/bloco_5/dia_3/ex-03_script.js
@@ -1,20 +1,26 @@
 //TAREFA 01
 
-let divButton = document.querySelector(".tarefa1");
-
 let createButton = document.createElement("button");
-createButton.className = "class-button";
+createButton.className = "class-button1";
 
+let divButton = document.querySelector(".tarefa1");
 divButton.appendChild(createButton);
 
-let button1 = document.querySelector(".class-button");
+let button1 = document.querySelector(".class-button1");
 button1.innerHTML = "Click on me";
 
-button1.addEventListener("click", function(){
-    alert("Voce clicou no botão!")
-})
-
+button1.addEventListener("click", function () {
+  alert("Voce clicou no botão!");
+});
 
 //TAREFA 02
 
+let productPrice = 59.99;
+let discount = 0.3;
 
+let button2 = document.querySelector(".class-button2");
+
+button2.addEventListener("click", function () {
+  let result = productPrice * discount;
+  alert("Desconto de " + result);
+});
