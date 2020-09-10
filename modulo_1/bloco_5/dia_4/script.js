cf4b9cf06e5a835013a4fcd454e06b420cd8659c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-10 19:29:33 -0300

    Guardando preferencias em local storages e as setando no proximo carregamento

--- modulo_1/bloco_5/dia_4/script.js
+++ modulo_1/bloco_5/dia_4/script.js
@@ -6,6 +6,7 @@ let colorBox1 = document.querySelector("#box1");
 button1.addEventListener("click", function () {
   let corFundo = colorBox1.value;
   paragraph.style.backgroundColor = corFundo;
+  localStorage.setItem("cor-fundo", corFundo);
 });
 
 let button2 = document.querySelector(".button-tc");
@@ -14,6 +15,7 @@ let colorBox2 = document.querySelector("#box2");
 button2.addEventListener("click", function () {
   let corTexto = colorBox2.value;
   paragraph.style.color = corTexto;
+  localStorage.setItem("cor-texto", corTexto);
 });
 
 let button3 = document.querySelector(".button-sl");
@@ -22,6 +24,7 @@ let textBox1 = document.querySelector("#box3");
 button3.addEventListener("click", function () {
   let newLineHeight = textBox1.value;
   paragraph.style.lineHeight = parseInt(newLineHeight) + "px";
+  localStorage.setItem("espaco-linha", newLineHeight);
 });
 
 let button4 = document.querySelector(".button-ft");
@@ -30,4 +33,19 @@ let textBox2 = document.querySelector("#box4");
 button4.addEventListener("click", function () {
   let newFontType = textBox2.value;
   paragraph.style.fontFamily = newFontType;
+  localStorage.setItem("tipo-fonte", newFontType);
 });
+
+if (localStorage.length != 0) {
+  let corFundo = localStorage.getItem("cor-fundo");
+  paragraph.style.backgroundColor = corFundo;
+
+  let corTexto = localStorage.getItem("cor-texto");
+  paragraph.style.color = corTexto;
+
+  let newLineHeight = localStorage.getItem("espaco-linha");
+  paragraph.style.lineHeight = newLineHeight + "px";
+
+  let newFontType = localStorage.getItem("tipo-fonte");
+  paragraph.style.fontFamily = newFontType;
+}
