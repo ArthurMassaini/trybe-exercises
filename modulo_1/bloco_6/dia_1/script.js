8c44f802358d1e78b184e2cf8619e1a98aab99b4
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-18 15:08:07 -0300

    Validando data

--- modulo_1/bloco_6/dia_1/script.js
+++ modulo_1/bloco_6/dia_1/script.js
@@ -37,3 +37,35 @@ function addEstados() {
   }
 }
 addEstados();
+
+function verificaData() {
+  let dia = '';
+  let mes = '';
+  let ano = '';
+  let data = document.querySelector('#data');
+  let re = new RegExp('[0-9]{2}/[0-9]{2}/[0-9]{4}');
+  if (data.value.search(re) == 0) {
+    for (let i = 0; i < 2; i += 1) {
+      dia = dia + data.value[i];
+    }
+    dia = parseInt(dia);
+    for (let i = 3; i < 5; i += 1) {
+      mes = mes + data.value[i];
+    }
+    mes = parseInt(mes);
+    for (let i = 6; i < 10; i += 1) {
+      ano = ano + data.value[i];
+    }
+    ano = parseInt(ano);
+
+    if (dia > 0 && dia <= 31 && mes > 0 && mes <= 12 && ano < 0) {
+      data.value = data.value;
+    } else {
+      alert('Data inválida!');
+      data.value = '';
+    }
+  }
+}
+
+let buttonEnviar = document.querySelector('#button-enviar');
+buttonEnviar.addEventListener('click', verificaData);
