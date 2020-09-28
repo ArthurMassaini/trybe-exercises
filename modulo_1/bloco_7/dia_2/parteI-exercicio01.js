de2031e51a255310c497baed381866797198ce26
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-28 18:18:11 -0300

    Movendo arquivos para apenas um

--- modulo_1/bloco_7/dia_2/parteI-exercicio01.js
+++
@@ -1,8 +0,0 @@
-const testObj = {
-  12: 'Namath',
-  16: 'Montana',
-  19: 'Unitas',
-};
-
-const playerNumber = 16;
-const player = testObj[playerNumber];
---
+++ modulo_1/bloco_7/dia_2/parteI.js
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------Exercício 01
+
+const testObj = {
+  12: 'Namath',
+  16: 'Montana',
+  19: 'Unitas',
+};
+
+const playerNumber = 16;
+const player = testObj[playerNumber];
+
+//-----------------------------------------------------------------------Exercício 02
+
+const myDog = {
+  name: 'Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+};
+
+myDog.name = 'Happy Coder';
