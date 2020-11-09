585bfe008f09392aeb8fedf59ea7f4b546a693fe
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-09 13:54:38 -0300

    Criando um componenete POKEMON para recuperar o nome, o tipo e a imagem

--- modulo_2/bloco_11/dia_2/my-pokedex/src/App.js
+++ modulo_2/bloco_11/dia_2/my-pokedex/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Pokemon from './Pokemon.js';
+import pokemons from './data.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Pokedex</h1>
+        <div className="pokedex">
+          {pokemons.map((element) => (
+            <Pokemon key={element.id} data={element} />
+          ))}
+        </div>
       </div>
     );
   }
